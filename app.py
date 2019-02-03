45dd2bc783435fc0769292a0f391972d669e05f9
bnguyen654 <me@bnguyen.org>
2019-02-03 13:46:12 -0800

    Update title of index.html to Lobby

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, emit
 import secrets
+from Games import Game
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.FLASK_SECRET_KEY;
 socketio = SocketIO(app)
+GAMES = {}
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@socketio.on("create")
+def create_game(data):
+    gm = Game(data['name'])
+    room = gm.id
+    GAMES[room] = gm
+    join_room(room)
+    emit('join_room', {'room'})
+
 
 
 if __name__ == "__main__":
